bf93b1b860e9db448509306187200bdb8bdadcdc
Marein <mail@marein.net>
2022-01-14 21:12:34 +0100

    Implement figure and widgets

--- app.js
+++ app.js
@@ -7,79 +7,141 @@ function shuffleArray(array) {
   }
 }
 
-angular.module("app", []).controller("controller", [
-  function () {
-    // this.value = 0;
-    // this.increment = function (by) {
-    //   this.value += by;
-    //   console.log("incremented by " + by + ", new value " + this.value);
-    // };
+angular
+  .module("app", [])
+  .controller("controller", [
+    function () {
+      this.pronouns = ["hij/zijn", "zij/haar", "die/diens", "hen/hun"];
 
-    this.pronouns = ["hij/zijn", "zij/haar", "die/diens", "hen/hun"];
+      this.clothingItems = [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+      ];
 
-    this.clothingItems = [
-      "01",
-      "02",
-      "03",
-      "04",
-      "05",
-      "06",
-      "07",
-      "08",
-      "09",
-      "10",
-      "11",
-      "12",
-    ];
+      this.physicalItemGroups = [
+        // ["borsten", "vagina", "baarmoeder/eierstokken"],
+        // ["penis/scrotum", "testikels"],
+        [
+          "borsten",
+          "vagina",
+          "baarmoeder/eierstokken",
+          "penis/scrotum",
+          "testikels",
+        ],
+      ];
 
-    this.physicalItemGroups = [
-      // ["borsten", "vagina", "baarmoeder/eierstokken"],
-      // ["penis/scrotum", "testikels"],
-      [
-        "borsten",
-        "vagina",
-        "baarmoeder/eierstokken",
-        "penis/scrotum",
-        "testikels",
-      ],
-    ];
+      shuffleArray(this.pronouns);
+      this.pronouns.push("andere");
+      shuffleArray(this.clothingItems);
+      this.physicalItemGroups.map(shuffleArray);
+      shuffleArray(this.physicalItemGroups);
 
-    shuffleArray(this.pronouns);
-    shuffleArray(this.clothingItems);
-    this.physicalItemGroups.map(shuffleArray);
-    shuffleArray(this.physicalItemGroups);
+      this.profile = {
+        name: null,
+        pronouns: new Set(),
+        identity: {},
+        sexuality: {},
+        expression: new Set(),
+        physical: new Set(),
+      };
 
-    this.profile = {
-      name: null,
-      pronouns: new Set(),
-      identity: {},
-      sexuality: {},
-      expression: new Set(),
-      physical: new Set(),
+      this.ui = {
+        plot: { x: null, y: null },
+      };
+
+      this.Array = Array;
+
+      this.widget = null;
+
+      this.plotMouseover = function (e) {
+        let rect = e.target.getBoundingClientRect();
+        this.ui.plot.x = ((e.clientX - rect.x) / rect.width) * 100;
+        this.ui.plot.y = ((e.clientY - rect.y) / rect.height) * 100;
+      };
+
+      this.plotMouseleave = function (e) {
+        this.ui.plot.x = this.ui.plot.y = null;
+      };
+
+      this.plotMouseclick = function (e, obj) {
+        obj.x = this.ui.plot.x;
+        obj.y = this.ui.plot.y;
+        this.ui.plot.x = this.ui.plot.y = null;
+      };
+    },
+  ])
+  .directive("dynamicPaddingCentering", function () {
+    return {
+      restrict: "A",
+      link: function (scope, elm, attrs) {
+        let parent = elm.parent();
+
+        scope.$watch(
+          function () {
+            return {
+              height: parent.prop("offsetHeight"),
+              width: parent.prop("offsetWidth"),
+            };
+          },
+          function (parentSize) {
+            let paddings = new Set();
+            let f = () => {
+              let padding = parseFloat(elm.css("padding-top") || 0);
+              let height = elm[0].clientHeight - padding;
+              let newPadding = (parentSize.height - height) / 2;
+              console.log(parentSize.height, padding, height, newPadding);
+              if (
+                Array.from(paddings).every((p) => Math.abs(p - newPadding) >= 5)
+              ) {
+                elm.css("padding-top", newPadding);
+                console.log(elm.css("padding-top"));
+                paddings.add(newPadding);
+                setTimeout(f, 20);
+              }
+            };
+            setTimeout(f);
+          },
+          true
+        );
+      },
     };
+  })
+  .directive("sameDimensionsAsId", function ($window) {
+    return {
+      restrict: "A",
+      link: function (scope, elm, attrs) {
+        let element = angular.element(
+          document.querySelector(attrs.sameDimensionsAsId)
+        );
 
-    this.ui = {
-      plot: { x: null, y: null },
+        angular.element($window).on("resize", function () {
+          elm.css("width", element.prop("offsetWidth"));
+          elm.css("height", element.prop("offsetHeight"));
+        });
+
+        // scope.$watch(
+        //   function () {
+        //     return {
+        //       width: element.prop("offsetWidth"),
+        //       height: element.prop("offsetHeight"),
+        //     };
+        //   },
+        //   function (elementSize) {
+        //     elm.css("width", elementSize.width);
+        //     elm.css("height", elementSize.height);
+        //   },
+        //   true
+        // );
+      },
     };
-
-    this.Array = Array;
-
-    this.view = "welcome";
-
-    this.plotMouseover = function (e) {
-      let rect = e.target.getBoundingClientRect();
-      this.ui.plot.x = ((e.clientX - rect.x) / rect.width) * 100;
-      this.ui.plot.y = ((e.clientY - rect.y) / rect.height) * 100;
-    };
-
-    this.plotMouseleave = function (e) {
-      this.ui.plot.x = this.ui.plot.y = null;
-    };
-
-    this.plotMouseclick = function (e, obj) {
-      obj.x = this.ui.plot.x;
-      obj.y = this.ui.plot.y;
-      this.ui.plot.x = this.ui.plot.y = null;
-    };
-  },
-]);
+  });
